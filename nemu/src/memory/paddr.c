b1baa88fc51448d2a54c9ef73f8114219ae57ac7
211220042-Sun XuDong <211220042@smail.nju.edu.cn>
2025-02-07 16:24:52 +0800

    pa2.2 feature: mtrace

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -51,6 +51,13 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
+#ifdef CONFIG_MTRACE
+   if(addr != cpu.pc){
+    printf("%d bytes data access " FMT_PADDR " at pc = " FMT_WORD "\n",len, addr, cpu.pc);
+  } else { 
+    // printf("instruction fetch " FMT_PADDR "\n", addr);
+  } 
+#endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
@@ -58,6 +65,13 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+#ifdef CONFIG_MTRACE
+  if(addr != cpu.pc){
+    printf("%d bytes data access " FMT_PADDR " at pc = " FMT_WORD "\n",len,  addr, cpu.pc);
+  } else { 
+    // printf("instruction fetch " FMT_PADDR "\n", addr);
+  }
+#endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
