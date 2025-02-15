0216841a3a2d5e3273d7cf46141d0d90d35c52cc
211220042-Sun XuDong <211220042@smail.nju.edu.cn>
2025-02-15 23:11:25 +0800

    pa2.3 feature: putc & clock

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,12 +5,86 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+int sprintf(char *out, const char *fmt, ...);
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  char buf[1024];
+
+  va_list args;
+  va_start(args,fmt);
+
+  int size = vsprintf(buf, fmt, args);
+
+  va_end(args);
+
+  for (int i = 0; buf[i] != '\0'; i++) {
+    putch(buf[i]);
+  }
+
+  return size;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  size_t len = strlen(fmt);
+  int i = 0, j = 0;
+  size_t bytes = 0;
+
+  // first part
+  if (fmt[i] == '%') {
+    j = i + 1;
+    if (fmt[j] == 'd') {
+      int n = va_arg(ap, int);
+      char buf[20];
+      to_string(n, buf);
+      strcpy(out, buf);
+      bytes += strlen(buf);
+    } else if (fmt[j] == 's') {
+      char *s = va_arg(ap, char*);
+      strcpy(out, s);
+      bytes += strlen(s);
+    } else {
+      panic("Not implemented");
+    }
+    i = j + 1;
+    j = i;
+  } else {
+    while (fmt[j] != '\0' && fmt[j] != '%') {j++;}
+    memcpy(out, fmt+i, j - i);
+    out[j - i] = '\0';
+    bytes += j - i;
+    i = j;
+  }
+
+  // rest character or conversion specification
+  while (i < len) {
+    if (fmt[i] == '%') {
+      j = i + 1;
+      if (fmt[j] == 'd') {
+        int n = va_arg(ap, int);
+        char buf[20];
+        to_string(n, buf);
+        strcat(out, buf);
+        bytes += strlen(buf);
+      } else if (fmt[j] == 's') {
+        char *s = va_arg(ap, char*);
+        strcat(out, s);
+        bytes += strlen(s);
+      } else {
+        panic("Not implemented");
+      }
+      i = j + 1;
+      j = i;
+    } else {
+      while (fmt[j] != '\0' && fmt[j] != '%') {j++;}
+      int str_len = strlen(out);
+      memcpy(out + str_len, fmt + i, j - i);
+      out[str_len + j - i] = '\0';
+      bytes += j - i;
+      i = j;
+    }
+  }
+
+  return bytes;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
@@ -41,7 +115,7 @@ int sprintf(char *out, const char *fmt, ...) {
     j = i;
   } else {
     while (fmt[j] != '\0' && fmt[j] != '%') {j++;}
-    memcpy(out, fmt, j - i);
+    memcpy(out, fmt+i, j - i);
     out[j - i] = '\0';
     bytes += j - i;
     i = j;
@@ -50,7 +124,7 @@ int sprintf(char *out, const char *fmt, ...) {
   // rest character or conversion specification
   while (i < len) {
     if (fmt[i] == '%') {
-      j = j + 1;
+      j = i + 1;
       if (fmt[j] == 'd') {
         int n = va_arg(args, int);
         char buf[20];
@@ -68,9 +142,9 @@ int sprintf(char *out, const char *fmt, ...) {
       j = i;
     } else {
       while (fmt[j] != '\0' && fmt[j] != '%') {j++;}
-      int len = strlen(out);
-      memcpy(out + len, fmt + i, j - i);
-      out[len + j - i] = '\0';
+      int str_len = strlen(out);
+      memcpy(out + str_len, fmt + i, j - i);
+      out[str_len + j - i] = '\0';
       bytes += j - i;
       i = j;
     }
